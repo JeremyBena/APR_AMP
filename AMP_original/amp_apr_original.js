a4d73209c8a95358081be4359f5660e708ae1dc6
JeremyBena <72076741+JeremyBena@users.noreply.github.com>
2021-03-12 10:59:03 +0100

    Update amp_apr_original.js

--- AMP_original/amp_apr_original.js
+++ AMP_original/amp_apr_original.js
@@ -10,18 +10,7 @@ define(['pipAPI', 'https://jeremybena.github.io/APR_AMP/AMP_original/AMP_paramet
 				nameForLogging : 'Black people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-				    {image : 'Francis.jpg'}, 
-					{image : 'b02.jpg'}, 
-					{image : 'b03.jpg'}, 
-					{image : 'b04.jpg'}, 
-					{image : 'b05.jpg'}, 
-					{image : 'b06.jpg'}, 
-					{image : 'b07.jpg'}, 
-					{image : 'b08.jpg'}, 
-					{image : 'b09.jpg'}, 
-					{image : 'b10.jpg'}, 
-					{image : 'b11.jpg'}, 
-					{image : 'b12.jpg'}]
+				    {image : 'Francis.jpg'}]
 
 			}, 
 			{
@@ -29,18 +18,11 @@ define(['pipAPI', 'https://jeremybena.github.io/APR_AMP/AMP_original/AMP_paramet
 				nameForLogging : 'White people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-				    {image : 'w01.jpg'}, 
-					{image : 'w02.jpg'}, 
-					{image : 'w03.jpg'}, 
-					{image : 'w04.jpg'}, 
-					{image : 'w05.jpg'}, 
-					{image : 'w06.jpg'}, 
-					{image : 'w07.jpg'}, 
-					{image : 'w08.jpg'}, 
-					{image : 'w09.jpg'}, 
-					{image : 'w10.jpg'}, 
-					{image : 'w11.jpg'}, 
-					{image : 'w12.jpg'}]
+				    {image : 'Control1.jpg'}, 
+					{image : 'Control2.jpg'}, 
+					{image : 'Control3.jpg'}, 
+					{image : 'Control4.jpg'}, 
+					{image : 'Control5.jpg'}]
 			}
 		],
 
@@ -48,7 +30,7 @@ define(['pipAPI', 'https://jeremybena.github.io/APR_AMP/AMP_original/AMP_paramet
 		{
 			nameForLogging : 'examplePrime', //Will be used in the logging
 			//An array of all media objects for this category.
-			mediaArray : [{image : 'Control1.jpg'}, {image : 'Control2.jpg'}, {image : 'Control3.jpg'}]
+			mediaArray : [{image : 'Control1.jpg'}, {image : 'Control2.jpg'}, {image : 'Control3.jpg'}, {image : 'Control4.jpg'}, {image : 'Control5.jpg'}]
 		},
 
 		base_url : {//Where are your images at?
