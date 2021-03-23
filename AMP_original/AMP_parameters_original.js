2b850b1e6c694e37e4c75a6e264f3e39bb90c07c
JeremyBena <72076741+JeremyBena@users.noreply.github.com>
2021-03-23 19:30:50 +0100

    Update AMP_parameters_original.js

--- AMP_original/AMP_parameters_original.js
+++ AMP_original/AMP_parameters_original.js
@@ -147,7 +147,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
 			'<color="ffffff">[Round 1 of nBlocks]</p></div>'+
 			'<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="ffffff"><br/>' + 
-			'Press the key <B>rightKey</B> if the targetCat is more rightAttribute than average. ' + 
+			'TEST the key <B>rightKey</B> if the targetCat is more rightAttribute than average. ' + 
 			'Hit the <b>leftKey</b> key if it is more leftAttribute than average.<br/><br/>' + 
 			'The items appear and disappear quickly.  ' + 
 			'Remember to ignore the item that appears before the targetCat and evaluate only the targetCat.<br/><br/></p>'  + 
