8fc6ac3038129f4f176046f09b0bc46605cc000b
JeremyBena <72076741+JeremyBena@users.noreply.github.com>
2021-03-23 17:55:25 +0100

    Update amp_apr_original_t2.js

--- AMP_original_t2/amp_apr_original_t2.js
+++ AMP_original_t2/amp_apr_original_t2.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://jeremybena.github.io/APR_AMP/AMP_original/AMP_parameters_original.js'], function(APIConstructor, ampExtension){
+define(['pipAPI', 'https://jeremybena.github.io/APR_AMP/AMP_original_t2/AMP_parameters_original_t2.js'], function(APIConstructor, ampExtension){
 
 	var API = new APIConstructor();
 	
