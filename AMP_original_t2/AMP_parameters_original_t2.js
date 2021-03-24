2dcdbaf54f11fc2d66d64d036a7cfa89cbaa6688
JeremyBena <72076741+JeremyBena@users.noreply.github.com>
2021-03-24 12:02:06 +0100

    Update AMP_parameters_original_t2.js

--- AMP_original_t2/AMP_parameters_original_t2.js
+++ AMP_original_t2/AMP_parameters_original_t2.js
@@ -141,39 +141,39 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 			'Now you will complete the first Visual Processing Task.<br/><br/>' + 
 			'You will see pairs of pictures flashed one after the other.<br/><br/>' + 
 			'The first picture shows a person, and the second shows a Chinese character.<br/><br/>' + 
-			'Your job is to judge the visual pleasantness of each Chinese character.<br/><br/>' +
-			'Of course, there are no right or wrong answers. Just report your own personal feelings as to whether each Chinese character looks relatively pleasant or unpleasant.<br/><br/>' +
+			'Your job is to judge whether each person is a pleasant individual or an unpleasant individual.<br/><br/>' +
+			'Of course, there are no right or wrong answers. Just report your intuition as to whether each person is a pleasant or unpleasant individual.<br/><br/>' +
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="ffffff"><br/><br/>' + 
 			'To proceed with the instructions, hit the <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
 			'<color="ffffff">[Instructions 1/3]</p></div>',
 			exampleBlockInst2: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="ffffff"><br/>' + 
 			'Put your middle or index fingers on the E and I keys of your keyboard.<br/><br/>' + 
-			'If a Chinese character looks more pleasant than average, press the E key on the left.<br/><br/>' + 
-			'If a Chinese character looks less pleasant than average, press the I key on the right.<br/><br/>' + 
+			'If you think someone is a more pleasant individual than average, press the E key on the left.<br/><br/>' + 
+			'If you think someone is a less pleasant individual than average, press the I key on the right.<br/><br/>' + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="ffffff"><br/><br/>' + 
 			'To proceed with the instructions, hit the <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
 			'<color="ffffff">[Instructions 2/3]</p></div>',
 			exampleBlockInst3: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="ffffff"><br/>' + 
-			'It is important to note that the pictures of people can sometimes bias people’s judgments of the Chinese characters.<br/><br/>' + 
-			'Because we are interested in how you can avoid being biased, please try your absolute best not to let the people bias your judgment of the Chinese characters!<br/><br/>' + 
-			'Give us an honest assessment of each Chinese character, regardless of the pictures that precede them.<br/><br/>' + 
+			'It is important to note that the Chinese characters can sometimes bias people’s judgments of the people.<br/><br/>' + 
+			'Because we are interested in how you can avoid being biased, please try your absolute best not to let the Chinese characters bias your judgment of the people!<br/><br/>' + 
+			'Give us an honest assessment of each person, regardless of the images that follow them.<br/><br/>' + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="ffffff"><br/><br/>' + 
 			'To proceed with the instructions, hit the <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
 			'<color="ffffff">[Instructions 3/3]</p></div>',
 			exampleBlockInst4: '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="ffffff"><br/>' + 
 			'To get a feel for the task, we will begin with 10 practice trials.<br/><br/>' + 
-			"Again, your task is to judge whether the Chinese characters look more pleasant or less pleasant than average by pressing either the 'E' or 'I' key.<br/><br/>" + 
+			"Again, your task is to judge whether the people are more pleasant individuals or less pleasant individuals than average by pressing either the 'E' or 'I' key.<br/><br/>" + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="ffffff"><br/><br/>' + 
 			'When you are ready to try a few practice responses, hit the <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
 			'<color="ffffff">[Practice trials]</p></div>',
 			firstBlockInst : '<div><p style="font-size:20px; text-align:left; margin-left:10px; font-family:arial"><color="ffffff"><br/>' + 
 			"Practice is now complete and the first Visual Processing Task will now begin.<br/><br/>" + 
-			"As a reminder, if you think a Chinese character looks more pleasant than average, press the 'E' key.<br/><br/>" + 
-			"If you think a Chinese character looks less pleasant than average, press the 'I' key.<br/><br/>" + 
+			"As a reminder, if you think someone is a more pleasant individual than average, press the 'E' key.br/><br/>" + 
+			"If you think someone is a less pleasant individual than average, press the 'I' key.<br/><br/>" + 
 			'<p style="font-size:16px; text-align:center; font-family:arial"><color="ffffff"><br/><br/>' + 
 			'Ready? Hit the <b>space bar</b>.</p>' + 
 			'<p style="font-size:12px; text-align:center; font-family:arial">' + 
